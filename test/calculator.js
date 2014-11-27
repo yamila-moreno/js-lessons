da9745baab8fdc50f2e1d7348a76f27f4aeb9296
Yamila Moreno <yamila.ms@gmail.com>
2014-11-27 22:49:04 +0100

    Add a bug to try explicit errors in travis
    just for fun...

--- test/calculator.js
+++ test/calculator.js
@@ -1,7 +1,7 @@
 describe('calculator', function () {
 
     it('calc', function () {
-        fnAdd = add01(3);
+        fnAdd = add02(3);
         fnSub = sub01(2);
         var res = calculator01(100, [fnAdd, fnSub]);
         expect(res).eql([103, 98]);
